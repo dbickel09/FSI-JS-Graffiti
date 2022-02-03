738f73a1e1bf7aaababbeaf39581c33c117ef585
Dan <bickel09@yahoo.com>
2022-02-02 19:07:45 -0600

    Flippin dogs

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ header.style.textAlign = 'left'
 
 let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++) {
-    dogImages[i].style.borderRadius = '50px'
-
+    dogImages[i].style.borderRadius = '150px'
 }
 
 let dogNames = document.querySelectorAll('.dog-name')
 for(let i = 0; i < dogNames.length; i++) {
     dogNames[i].style.textAlign = 'right'
-
 }
+
